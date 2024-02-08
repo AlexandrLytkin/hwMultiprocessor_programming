80227ba02a620c876bdbcd6bc41c1f5815ec3896
AlexandrLytkin <111504959+AlexandrLytkin@users.noreply.github.com>
2024-02-08 15:06:35 +0300

    Добавил проверку баланса

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
         ("product2", "receipt", 150),
         ("product1", "shipment", 30),
         ("product3", "receipt", 200),
-        ("product2", "shipment", 500)
+        ("product2", "shipment", 50)
     ]
 
     # Запускаем обработку запросов
